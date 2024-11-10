53ad588546368ed8d69aba065ebd75ffa3ef678b
TychicusThomas <156248950+TychicusThomas@users.noreply.github.com>
2024-11-10 11:14:11 -0600

    Update new_user.php

--- new_user.php
+++ new_user.php
@@ -1,17 +1,21 @@
-<?php
-// session start here...
-
-// get all 3 strings from the form (and scrub w/ validation function)
-
-// make sure that the two password values match!
-
-
-// create the password_hash using the PASSWORD_DEFAULT argument
-
-// login to the database
-
-// make sure that the new user is not already in the database
-
-// insert username and password hash into db (put the username in the session
-// or make them login)
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Register New User</title>
+</head>
+<body>
+    <form action="register.php" method="POST">
+        <label for="user">Username:</label> 
+        <input type="text" id="user" name="user" required><br>
+        
+        <label for="pwd">Password:</label> 
+        <input type="password" id="pwd" name="pwd" minlength="6" required><br>
+        
+        <label for="repeat">Repeat Password:</label> 
+        <input type="password" id="repeat" name="repeat" minlength="6" required><br>
+        
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
